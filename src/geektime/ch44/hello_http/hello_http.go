840e9ba9f1faf95466cd17c1d46245cbff405f26
sptuan <sptuan@steinslab.io>
2020-09-16 16:29:17 +0800

    learn simple http server

---
+++ src/geektime/ch44/hello_http/hello_http.go
@@ -0,0 +1,19 @@
+package hello_http
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!\n")
+	})
+	http.HandleFunc("/time/", func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
+		timeStr := fmt.Sprintf("{\"time\": \"%s\"}", t)
+		w.Write([]byte(timeStr))
+	})
+	http.ListenAndServe(":8080", nil)
+}
